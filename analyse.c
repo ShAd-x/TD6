c54150164abe1040ff0218ed48c5a82388cedea9
ShAd-x <riValofking@gmail.com>
2021-10-19 16:37:17 +0200

    initial commit

---
+++ analyse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argn, char *argv[], char *arge[]) {
+    //printf("%d\n", argn);
+    if(argn < 2){
+        printf("Un argument est requis\n");
+        return 2;
+    } else {
+        //if(fichier introuvable) return 1;
+        //else prog; return 0;
+    }
+    return 0;
+}
