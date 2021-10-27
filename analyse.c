08b3a336b851aadc4b1282589e54b225da1fc116
Adrien222 <74194175+Adrien222@users.noreply.github.com>
2021-10-27 17:11:47 +0200

    maj comm

--- analyse.c
+++ analyse.c
@@ -5,42 +5,39 @@
 #include <fcntl.h>
 
 /* prototypes des fonctions externes */
-// float moyenne(float[]);  Inutile car pas appellée dans cette classe.
-// float mini(float[]);     Inutile car pas appellée dans cette classe.
-// float maxi(float[]);     Inutile car pas appellée dans cette classe.
 void affiche(float[]);
 
 int main(int argn, char *argv[], char *arge[]) {
     float *tablo = (float*) malloc(150 * sizeof(float));
     float Tab[50];
     if(argn < 2){
-        printf("Un argument est requis\n");
+        printf("Un argument est requis\n"); // vérification qu'un chemin de fichier a été fournit
         return 1;
     } else {
         int file = open(argv[1], O_RDONLY);
-        if (file != -1) {
+        if (file != -1) { // check si le fichier est accessible
             for(int  CompteurTableau= 1; CompteurTableau<150; CompteurTableau++){
                 *tablo = read(file, tablo, 150*sizeof(float));
             }
             printf("Espece Iris Setosa :\n");
-            for(int CompteurTabSetosa=1, y=0; CompteurTabSetosa<=50; CompteurTabSetosa++, y++){ // pas bonne valeur pour le 1er (J'ai mis 50 et ça donne des trucs)
+            for(int CompteurTabSetosa=1, y=0; CompteurTabSetosa<=50; CompteurTabSetosa++, y++){ // permet d'afficher les valeurs concernant L'Iris Setosa
                 Tab[y] = tablo[CompteurTabSetosa];
             }
             affiche(Tab);
 
             printf("Espece Iris Versicolor :\n");
-            for(int CompteurTabVersicolor=50, y=0; CompteurTabVersicolor<=99; CompteurTabVersicolor++, y++){
+            for(int CompteurTabVersicolor=50, y=0; CompteurTabVersicolor<=99; CompteurTabVersicolor++, y++){ // permet d'afficher les valeurs concernant L'Iris Versicolor
                 Tab[y] = tablo[CompteurTabVersicolor];
             }
             affiche(Tab);
 
             printf("Espece Iris Virginica :\n");
-            for(int CompteurTabVirginica=100, y=0; CompteurTabVirginica<=149; CompteurTabVirginica++, y++){
+            for(int CompteurTabVirginica=100, y=0; CompteurTabVirginica<=149; CompteurTabVirginica++, y++){ // permet d'afficher les valeurs concernant L'Iris Virginica
                 Tab[y] = tablo[CompteurTabVirginica];
             }
             affiche(Tab);
         } else {
-            perror (argv[1]);
+            perror (argv[1]); // si le fichier n'est pas accessible, affiche l'erreur
             return 2;
         }
     }
